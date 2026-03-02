13573b3f70260715e427130ff5bf64e6edf0633d
sanklss <sanklsswt2@yandex.ru>
2026-03-02 14:10:37 +0300

    Добавление класса ApplicationView

---
+++ Homework1/ApplicationView.cs
@@ -0,0 +1,41 @@
+﻿namespace Homework1
+{
+    public class ApplicationView
+    {
+        public string WriteLogCommand { get; } = "1";
+        public string ExitCommand { get; } = "2";
+
+        private Dictionary<string, string> _commands;
+
+        public ApplicationView()
+        {
+            _commands = new Dictionary<string, string>()
+            {
+                { WriteLogCommand, "Написать лог" },
+                { ExitCommand, "Выйти" }
+            };
+        }
+
+        public void ShowMenu()
+        {
+            foreach (KeyValuePair<string, string> command in _commands)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{command.Value} - {command.Key}");
+                Console.ResetColor();
+            }
+        }
+
+        public void ShowInvalidCommandMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Неверная команда!");
+            Console.ResetColor();
+        }
+
+        public string GetUserInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
