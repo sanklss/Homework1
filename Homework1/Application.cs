2f055d02857c323efc3c54eaa1538c3704acf47e
sanklss <sanklsswt2@yandex.ru>
2026-03-02 14:21:09 +0300

    Добавление и реализация класса Application

---
+++ Homework1/Application.cs
@@ -0,0 +1,43 @@
+﻿namespace Homework1
+{
+    public class Application
+    {
+        private ApplicationView _applicationView;
+
+        private string _writeLogCommand;
+        private string _exitCommand;
+
+        public Application()
+        {
+            _applicationView = new ApplicationView();
+
+            _writeLogCommand = _applicationView.WriteLogCommand;
+            _exitCommand = _applicationView.ExitCommand;
+        }
+
+        public void Run()
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                _applicationView.ShowMenu();
+                string userInput = _applicationView.GetUserInput();
+
+                if (userInput == _writeLogCommand)
+                {
+                    string message = _applicationView.NewMethod();
+                    Logger.WriteLog(message);
+                }
+                else if (userInput == _exitCommand)
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    _applicationView.ShowInvalidCommandMessage();
+                }
+            }
+        }
+    }
+}
