e55a57ca621745ed7a0f443f766d1d96122a9fba
sanklss <sanklsswt2@yandex.ru>
2026-03-02 14:01:36 +0300

    Создание и реализация класса Logger

---
+++ Homework1/Logger.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework1
+{
+    public class Logger
+    {
+        private static string _logFilePath;
+
+        static Logger()
+        {
+            _logFilePath = "C:\\Users\\repos";
+        }
+
+        public static void WriteLog(string log)
+        {
+            const string LogSeparator = "> ";
+
+            Console.WriteLine(_logFilePath + LogSeparator + log);
+        }
+    }
+}
---
+++ Homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.WriteLog("ddddd");
+        }
+    }
+}
