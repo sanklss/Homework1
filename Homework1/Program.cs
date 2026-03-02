77a9905c5490b39a914dd90eea44d8986b37cf01
sanklss <sanklsswt2@yandex.ru>
2026-03-02 14:22:32 +0300

    Запуск программы через метод Run

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Logger.WriteLog("ddddd");
+            Application application = new Application();
+            application.Run();
         }
     }
 }
